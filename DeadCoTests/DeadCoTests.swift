ef6113df4e2d1eeead0b5d4ab66c19bdf64636c8
Elaine Ou <elaine.ou@gmail.com>
2015-11-01 17:22:24 -0800

    Initial Commit

---
+++ DeadCoTests/DeadCoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DeadCoTests.swift
+//  DeadCoTests
+//
+//  Created by Elaine Ou on 11/1/15.
+//  Copyright © 2015 Elaine Ou. All rights reserved.
+//
+
+import XCTest
+@testable import DeadCo
+
+class DeadCoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
