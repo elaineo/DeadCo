b1809077c82a8bfc36aeedaf7116e679fcfc4218
Elaine Ou <elaine.ou@gmail.com>
2015-11-01 19:12:04 -0800

    dump

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'DeadCo' do
+  pod "Koloda"
+end
---
+++ Pods/Target Support Files/Pods-DeadCo/Pods-DeadCo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DeadCoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DeadCoVersionString[];
+
